a58a72e41469add53a2b33bf093606b42f82ff9d
Achi054 <sujith.acharya054@yahoo.com>
2023-01-28 12:10:11 +0530

    Worker setup

---
+++ WorkflowCore/Program.cs
@@ -0,0 +1,10 @@
+using WorkflowCore;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
